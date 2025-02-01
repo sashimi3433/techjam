953264a6a55ff8dd762c643895ef8f835cce1149
酒井 風志 <100179630+sashimi3433@users.noreply.github.com>
2025-02-01 13:19:03 +0900

    ベース

--- home/views.py
+++ home/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import views
 
-# Create your views here.
 
 def index(request):
-    # return render(request, 'index.html')
-    return HttpResponse('Hello, world!')
+    return render(request, 'home/index.html')
